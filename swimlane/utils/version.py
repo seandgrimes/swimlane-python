10612351a09d5250ba6ed2905fe2f87abd89d01e
Sean Grimes <sean@codemunkey.net>
2023-01-20 14:01:50 -0600

    Revert "Other changes"
    This reverts commit 8fd372ce8b5441f9052bc21c7d73637faa3807d9.

--- swimlane/utils/version.py
+++ swimlane/utils/version.py
@@ -98,6 +98,6 @@ def get_package_version():
          str: Installed swimlane lib package version, or 0.0.0.dev if not fully installed
     """
     try:
-        return distribution("swimlane").version
+        return distribution(__name__.split('.')[0]).version
     except PackageNotFoundError:
         return '0.0.0.dev'
